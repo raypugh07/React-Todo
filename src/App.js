212252ae393ceaeb5279b96f06249553104f7f11
Your Name <you@example.com>
2019-04-24 17:52:02 -0500

    mvp day one,app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList';
+import TodoForm from './components/TodoComponents/TodoForm';
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          task: 'Go to gym',
+          id:1,
+          completed: false
+        },
+        {
+          task: 'Lay on couch and do nothing',
+          id: 2,
+          completed: false
+        }
+      ],
+      todo: ''
+    };
+  }
+  
+  add = e => {
+    e.preventDefault();
+    const todos = this.state.todos.slice();
+    todos.push({ task: this.state.todo, completed: false, id: Date.now() });
+    this.setState({ todos, todo: '' });
+  };
+
+  change = e => this.setState({ [e.target.name]: e.target.value });
+
+  
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <TodoList
+         todos={this.state.todos}
+        />
+        <TodoForm
+          value={this.state.todo}
+          handleChange={this.change}
+          handleAdd={this.add}
+         
+        />
       </div>
     );
   }
